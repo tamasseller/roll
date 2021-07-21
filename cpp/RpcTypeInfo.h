a0b75b53f4ca40c465d004c62e88b0169c0da05f
Tamás Seller <seller.tamas@gmail.com>
2021-07-21 23:13:44 +0200

    Fix namespace issue

--- cpp/RpcTypeInfo.h
+++ cpp/RpcTypeInfo.h
@@ -128,10 +128,10 @@ template<size_t n> class CTStr;
 template<class... Args> struct TypeInfo<Call<Args...>> 
 {
 	template<class S> static constexpr inline decltype(auto) writeName(S&& s) { return writeSignature<Args...>(s); }
-	template<class S> static inline bool write(S& s, const Call<Args...> &v) { return VarUint4::write(s, v.id); }
-	template<class S> static inline bool read(S& s, Call<Args...> &v) { return VarUint4::read(s, v.id); }
-	template<class S> static inline bool skip(S& s) { return VarUint4::skip(s); }
-	static constexpr inline size_t size(const Call<Args...> &v) { return VarUint4::size(v.id); }
+	template<class S> static inline bool write(S& s, const Call<Args...> &v) { return ::rpc::VarUint4::write(s, v.id); }
+	template<class S> static inline bool read(S& s, Call<Args...> &v) { return ::rpc::VarUint4::read(s, v.id); }
+	template<class S> static inline bool skip(S& s) { return ::rpc::VarUint4::skip(s); }
+	static constexpr inline size_t size(const Call<Args...> &v) { return ::rpc::VarUint4::size(v.id); }
 	static constexpr inline bool isConstSize() { return false; }
 };
 
@@ -164,7 +164,7 @@ template<class T> struct CollectionTypeBase: CollectionPlaceholder<T>
 	template<class S> static inline bool skip(S& s)
     {
         uint32_t count;
-        if(!VarUint4::read(s, count))
+        if(!::rpc::VarUint4::read(s, count))
             return false;
 
         while(count--)
@@ -203,7 +203,7 @@ template<class C, class T> struct StlCompatibleCollectionTypeBase: CollectionTyp
             }
         }
 
-        return contentSize + VarUint4::size(count);
+        return contentSize + ::rpc::VarUint4::size(count);
     }
 };
 
